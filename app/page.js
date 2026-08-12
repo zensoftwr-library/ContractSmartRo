aba7694ff9018ca539f889a8a074c2a89db7e18c
zensoftwr-library <zensoftwr@gmail.com>
2026-08-13 00:42:17 +0300

    update

--- app/page.js
+++ app/page.js
@@ -1157,7 +1157,7 @@ export default function Home() {
             <span className="text-slate-800">|</span>
             <Link href="/contact" className="text-xs text-slate-400 hover:text-white transition">Contact</Link>
             <span className="text-slate-800">|</span>
-            <Link href="/despre-noi" className="hover:text-[#8ba888] transition font-bold">Despre Noi</Link>
+            <Link href="/despre-noi" className="text-xs text-slate-400 hover:text-white transition">Despre Noi</Link>
             <span>•</span>
             
             {!user ? (
